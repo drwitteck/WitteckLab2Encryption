4f0901038d5a06194d090d8eec1fe6db0e25058d
Derek Witteck <drwitteck@gmail.com>
2018-02-08 17:10:44 -0500

    Styling and onClickListeners for the buttons

--- app/src/main/java/com/drwitteck/wittecklab2encryption/MainActivity.java
+++ app/src/main/java/com/drwitteck/wittecklab2encryption/MainActivity.java
@@ -1,13 +1,39 @@
 package com.drwitteck.wittecklab2encryption;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button encryptButton, decryptButton;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        encryptButton = (Button) findViewById(R.id.buttonEncrypt);
+        decryptButton = (Button) findViewById(R.id.buttonDecrypt);
+
+        encryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //encrypt text
+            }
+        });
+
+        decryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //decrypt text
+            }
+        });
     }
 }
