751f72cf08914e84d31e7fb888e03221dc9cc0b7
Derek Witteck <drwitteck@gmail.com>
2018-03-04 23:40:21 -0500

    Deleted encryption code that was left over from branch merge

--- app/src/main/java/com/drwitteck/wittecklab2encryption/MainActivity.java
+++ app/src/main/java/com/drwitteck/wittecklab2encryption/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     boolean requested;
     RSA rsa;
 
-    final static String METHOD = "RSA";
-
     private final static String METHOD = "RSA";
 
 
@@ -148,68 +146,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //
 //        return ndefMessage;
 //    }
-
-    public void requestKeys() throws InvalidKeySpecException, NoSuchAlgorithmException {
-        cursor = getContentResolver()
-                .query(Uri.parse("content://com.drwitteck.wittecklab2encryption.encryptionprovider")
-                        , null, null, null, null);
-
-        assert cursor != null;
-        cursor.moveToNext();
-        cursor.close();
-
-        /*
-        Extract the public key from the cursor, decode the string, then re-encode using X509 standard
-        to pass to encrypt function
-         */
-        publicKeyString = cursor.getString(0);
-        byte[] publicBytes = Base64.decode(publicKeyString, Base64.DEFAULT);
-        /*Implementation of this code was reviewed on StackOverflow*/
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicBytes);
-        KeyFactory keyFactory = KeyFactory.getInstance(METHOD);
-        publicKey = keyFactory.generatePublic(keySpec);
-
-        /*
-        Extract the private key from the cursor, decode the string, then re-encode using X509 standard
-        to pass to decrypt function
-         */
-        privateKeyString = cursor.getString(1);
-        byte[] privateBytes = Base64.decode(privateKeyString, Base64.DEFAULT);
-        /*Implementation of this code was reviewed on StackOverflow*/
-        PKCS8EncodedKeySpec keySpecPrivate = new PKCS8EncodedKeySpec(privateBytes);
-        KeyFactory keyFactoryPrivate = KeyFactory.getInstance(METHOD);
-        privateKey = keyFactoryPrivate.generatePrivate(keySpecPrivate);
-
-        decryptButton.setEnabled(false);
-
-        Toast.makeText(this, "Keys requested and saved.", Toast.LENGTH_SHORT).show();
-    }
-
-    public void encryptText() throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException,
-            NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException {
-        if(TextUtils.isEmpty(editText.getText().toString())){
-            editText.setError("Please enter text to encrypt.");
-        } else {
-            /*
-            Send the user entered string to be converted to bytes. Then send it and the public key
-            to the encrypt method.
-             */
-            bytes = rsa.encrypt(publicKey, rsa.stringToBytes(editText.getText().toString()));
-            editText.setText(rsa.bytesToString(bytes));
-
-            decryptButton.setEnabled(true);
-        }
-    }
-
-    public void decryptText() throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException,
-            NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException {
-        try {
-            editText.setText(rsa.decrypt(privateKey, bytes));
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException |
-                BadPaddingException | InvalidKeyException e) {
-            e.printStackTrace();
-        }
-
-        decryptButton.setEnabled(false);
-    }
 }
